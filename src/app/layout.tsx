ce00cd0152c39b226ef5434868aafde4aaf8addf
vinimedeiros13 <vinimedeiros7@outlook.com>
2022-12-12 07:31:54 -0300

    feat: add microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,21 @@ export default function RootLayout({ children }: RootLayoutProps) {
           href="https://cdn.worldvectorlogo.com/logos/next-js.svg"
           type="image/x-icon"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+         (function(c,l,a,r,i,t,y){
+             c[a] = c[a] || function () { (c[a].q = c[a].q || 
+             []).push(arguments) };
+             t=l.createElement(r);
+             t.async=1;
+             t.src="https://www.clarity.ms/tag/"+i;
+             y=l.getElementsByTagName(r)[0];
+             y.parentNode.insertBefore(t,y);
+         })(window, document, "clarity", "script", "ex63wnhjkq");`,
+          }}
+        />
+        ;
       </head>
       <body>
         <ChakraProvider theme={theme}>
